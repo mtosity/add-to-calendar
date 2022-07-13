edeb972d2ebe89798ae2f4999411c07fd2624485
mtosity <mtosity@gmail.com>
2022-07-13 17:51:06 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { AddButton } from "./components/AddButton";
+import "add-to-calendar-button/assets/css/atcb.css";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="App">
+        <header className="App-header">
+          <Typography variant="h3" style={{ marginLeft: 16 }}>
+            Generate calendar create links
+          </Typography>
+        </header>
+        <AddButton />
+      </div>
+    </LocalizationProvider>
   );
 }
 
---
+++ src/components/AddButton.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { atcb_action } from "add-to-calendar-button";
+import { TextField, Button } from "@mui/material";
+import { Box } from "@mui/system";
+import { DateTimePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+
+export const AddButton = () => {
+  const [data, setData] = React.useState({
+    startISO: new Date().toISOString(),
+    endISO: new Date().toISOString(),
+    text: "",
+    details: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const startDate = dayjs(data.startISO).format("YYYY-MM-DD"),
+      endDate = dayjs(data.endISO).format("YYYY-MM-DD");
+
+    const startTime = dayjs(data.startISO).format("HH:mm"),
+      endTime = dayjs(data.endISO).format("HH:mm");
+
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    atcb_action({
+      name: data.text,
+      description: data.details,
+      startDate,
+      endDate,
+      startTime,
+      endTime,
+      timeZone,
+      options: [
+        "Apple",
+        "Google",
+        "iCal",
+        "Microsoft365",
+        "Outlook.com",
+        "Yahoo",
+      ],
+      trigger: "click",
+      iCalFileName: "Reminder-Event",
+    });
+  };
+
+  return (
+    <form style={{ padding: 16 }} onSubmit={onSubmit}>
+      <Box paddingBottom={2}>
+        <TextField
+          label="Title / Name"
+          variant="outlined"
+          onChange={(e) => setData({ ...data, text: e.target.value })}
+          fullWidth
+          required
+        />
+      </Box>
+
+      <Box paddingBottom={2}>
+        <TextField
+          label="Description"
+          variant="outlined"
+          multiline
+          fullWidth
+          rows={4}
+          onChange={(e) => setData({ ...data, details: e.target.value })}
+        />
+      </Box>
+
+      <Box paddingBottom={2}>
+        <DateTimePicker
+          label="Start"
+          onChange={(date) =>
+            setData({ ...data, startISO: new Date(date).toISOString() })
+          }
+          renderInput={(params) => <TextField {...params} />}
+          required
+        />
+        <span style={{ marginLeft: 4, marginRight: 4 }} />
+        <DateTimePicker
+          label="End"
+          onChange={(date) =>
+            setData({ ...data, endISO: new Date(date).toISOString() })
+          }
+          renderInput={(params) => <TextField {...params} />}
+          required
+        />
+      </Box>
+
+      <Box paddingBottom={2}>
+        <Button variant="contained" type="submit">
+          Generate
+        </Button>
+      </Box>
+    </form>
+  );
+};
